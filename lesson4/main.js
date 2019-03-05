708759480763bd206bd3dc90cb1ce30d52a56b13
Arkadiy Pryadko <arkadij.pryadko@gmail.com>
2019-03-05 21:00:38 +0200

    урок 4

---
+++ lesson4/main.js
@@ -0,0 +1,24 @@
+"use strict";
+//alert('Hello, world');
+//alert('Hello');// comment 1
+console.log('Hello, console');
+
+/*
+comment 2
+ */
+// var messageP;
+// var count = 2;
+// var message1 = 'fff', label, message2 = 'kkk';
+//
+//
+// messageP = 'Текс сообщения';
+
+
+var num1, num2, result;
+
+num1 = +prompt('Введите первое число, 0');
+num2 = +prompt('Введите второе число, 0');
+
+result = num1 + num2;
+
+alert('Результат сложения: ' + result);
