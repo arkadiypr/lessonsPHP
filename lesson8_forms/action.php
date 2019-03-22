ab96d80052866670ed7a434e4b935ebef0114ef2
Arkadiy Pryadko <arkadij.pryadko@gmail.com>
2019-03-22 21:08:59 +0200

    урок 8 - class Form

---
+++ lesson8_forms/action.php
@@ -0,0 +1,31 @@
+<?php
+
+$errors = [];
+
+if (empty($_POST['email'])) {
+    $errors['email'] = 'Введите email';
+}
+
+
+if (empty($_POST['password'])) {
+    $errors['password'] = 'Введите пароль';
+}
+
+
+if (empty($_POST['passwordConfirmation'])) {
+    $errors['passwordConfirmation'] = 'Введите подтверждение пароля';
+} elseif ($_POST['password'] != $_POST['passwordConfirmation']) {
+    $errors['passwordConfirmation'] = 'Пароли не совпадают';
+}
+
+if ($errors) {
+    include 'index.php';
+    exit();
+}
+
+$file = fopen('user.txt', 'a');
+$line = $_POST['email'] . "\t" . $_POST['password'] . "]n";
+fputs($file, $line);
+fclose($file);
+
+header('Location: thanks.html');
